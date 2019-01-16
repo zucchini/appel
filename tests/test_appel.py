ecc718d4acf32335f3fa2a098aec62c760342e2e
Cem Gokmen <cem@cemgokmen.com>
2019-01-15 20:37:49 -0500

    Patch nonexistent tests

--- tests/test_appel.py
+++ tests/test_appel.py
@@ -24,9 +24,6 @@ class TestAppel(unittest.TestCase):
     def test_command_line_interface(self):
         """Test the CLI."""
         runner = CliRunner()
-        result = runner.invoke(cli.main)
-        assert result.exit_code == 0
-        assert 'appel.cli.main' in result.output
-        help_result = runner.invoke(cli.main, ['--help'])
+        help_result = runner.invoke(cli.cli, ['--help'])
         assert help_result.exit_code == 0
         assert '--help  Show this message and exit.' in help_result.output
